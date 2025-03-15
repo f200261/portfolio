704c870b135692554fc8347516d76106e46a9f2b
f200261 <f200261@cfd.nu.edu.pk>
2025-03-16 00:00:31 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import chatHandler from "./api/chat.js";
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,15 +10,18 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Enable CORS
+app.use(cors());
+
 // Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 // API endpoint for chatbot
 app.use("/api", chatHandler);
 
 // Serve the index.html file for all other routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
 app.listen(port, () => {
